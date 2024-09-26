78a4104fbfed2c8a776d04557d205359cd0b376f
just_luis <luis_edward340@live.com>
2024-09-26 16:42:50 -0700

    Cleaned up database.py and implemented queries in order to use them at any time to create tables and inserting into the DB.

--- database.py
+++ database.py
@@ -1,18 +1,27 @@
 import sqlite3
+#create queries in order to use them at any time
+inventory_table="CREATE TABLE IF NOT EXISTS inventory(machine_id TEXT PRIMARY KEY,name TEXT,description TEXT,quantity INT,price FLOAT,total FLOAT);"
+insert_machine="INSERT INTO machines VALUES (?,?,?,?,?,?)"
 
-connection = sqlite3.connect("machine.db")
-cursor = sqlite3.Cursor
-with connection:
-    connection.execute(
-        "CREATE TABLE IF NOT EXISTS machines(machine_number TEXT PRIMARY KEY, name TEXT, description TEXT, quantity INT, price FLOAT, total FLOAT);")
+#create a function that returns connection to the database provided
+def connection():
+    return sqlite3.connect("machine.db")
+# def cursor():
+#     return sqlite3.Cursor
 
-machine_list = [
-    ("CF-3163", "flat bench","pinche flat bench duh", 2, 329.45, 658.9)
-]
-connection.executemany("INSERT INTO machines VALUES (?,?,?,?,?,?)", machine_list)
+def create_table(connection):
+    with connection:
+        connection.execute(inventory_table)
+#def add_machine(connection, machine_id,name, description ,quantity , price, total):
+
+
+# machine_list = [
+#     ("CF-3163", "flat bench","pinche flat bench duh", 2, 329.45, 658.9)
+#     ]
+
 #printing the values in a table by using the cursor
 #for row in cursor.execute("select * from machine_order"):
     #print(row)
 
-connection.commit()
-connection.close()
+connection().commit()
+connection().close()
